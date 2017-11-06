0af188106f5eacea5a411e6cc1adfdad437e0dd3
Aaron Lam <aaron.y.lam@sjsu.edu>
2017-11-05 16:47:33 -0800

    Add files via upload

---
+++ cross_reference/Debug/src/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/xref.cpp 
+
+OBJS += \
+./src/xref.o 
+
+CPP_DEPS += \
+./src/xref.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -I"C:\Users\Young\workspace\courselib\src" -O0 -g3 -Wall -c -fmessage-length=0 -std=gnu++11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
